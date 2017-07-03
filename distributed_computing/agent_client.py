02d1be0068fee3889bf9b45984fc3e2301983301
Frederic Marvin Abraham <f.abraham@campus.tu-berlin.de>
2017-07-04 00:53:14 +0200

    Finished Server and client

---
+++ distributed_computing/agent_client.py
@@ -0,0 +1,82 @@
+'''In this file you need to implement remote procedure call (RPC) client
+
+* The agent_server.py has to be implemented first (at least one function is implemented and exported)
+* Please implement functions in ClientAgent first, which should request remote call directly
+* The PostHandler can be implement in the last step, it provides non-blocking functions, e.g. agent.post.execute_keyframes
+ * Hints: [threading](https://docs.python.org/2/library/threading.html) may be needed for monitoring if the task is done
+'''
+
+import weakref
+import xmlrpclib
+import threading
+import sys
+import os
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
+from keyframes import hello
+
+class PostHandler(object):
+    '''the post hander wraps function to be excuted in paralle
+    '''
+    def __init__(self, obj):
+        self.proxy = weakref.proxy(obj)
+
+    def keyFrame_thread(self, keyframes):
+        self.proxy.execute_keyframes(keyframes)
+
+    def set_transform_thread(self, effector_name, transform):
+        self.proxy.set_transform(effector_name, transform)
+
+    def execute_keyframes(self, keyframes):
+        '''non-blocking call of ClientAgent.execute_keyframes'''
+        thread = threading.Thread(target=self.keyFrame_thread, args=[keyframes])
+        thread.start()
+
+    def set_transform(self, effector_name, transform):
+        '''non-blocking call of ClientAgent.set_transform'''
+        thread = threading.Thread(target=self.set_transform_thread, args=[effector_name, transform])
+        thread.start()
+
+
+class ClientAgent(object):
+    '''ClientAgent request RPC service from remote server
+    '''
+    # YOUR CODE HERE
+    def __init__(self):
+        self.MyProxy = xmlrpclib.ServerProxy("http://localhost:9000")
+        self.post = PostHandler(self.MyProxy)
+
+    def get_angle(self, joint_name):
+        '''get sensor value of given joint'''
+        return self.MyProxy.get_angle(joint_name)
+    
+    def set_angle(self, joint_name, angle):
+        '''set target angle of joint for PID controller
+        '''
+        self.MyProxy.set_angle(joint_name, angle)
+
+    def get_posture(self):
+        '''return current posture of robot'''
+        # That sadly cant work because the recognize posture agent isn't part of the inheritance list
+
+    def execute_keyframes(self, keyframes):
+        '''excute keyframes, note this function is blocking call,
+        e.g. return until keyframes are executed
+        '''
+        self.post.execute_keyframes(keyframes)
+
+    def get_transform(self, name):
+        '''get transform with given name
+        '''
+        return self.MyProxy.get_transform(name)
+
+    def set_transform(self, effector_name, transform):
+        '''solve the inverse kinematics and control joints use the results
+        '''
+        self.post.set_transform(effector_name, transform)
+
+if __name__ == '__main__':
+    agent = ClientAgent()
+    print(agent.execute_keyframes(hello()))
+    print("Test")
+
+
---
+++ distributed_computing/agent_server.py
@@ -0,0 +1,86 @@
+'''In this file you need to implement remote procedure call (RPC) server
+
+* There are different RPC libraries for python, such as xmlrpclib, json-rpc. You are free to choose.
+* The following functions have to be implemented and exported:
+ * get_angle
+ * set_angle
+ * get_posture
+ * execute_keyframes
+ * get_transform
+ * set_transform
+* You can test RPC server with ipython before implementing agent_client.py
+'''
+
+# add PYTHONPATH
+import os
+import sys
+import SimpleXMLRPCServer
+from collections import OrderedDict
+import threading
+import numpy as np
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
+
+from inverse_kinematics import InverseKinematicsAgent
+
+
+class ServerAgent(InverseKinematicsAgent):
+    '''ServerAgent provides RPC service
+    '''
+    
+    def get_angle(self, joint_name):
+        '''get sensor value of given joint'''
+        return self.perception.joint[joint_name]
+    
+    def set_angle(self, joint_name, angle):
+        '''set target angle of joint for PID controller
+        '''
+        if joint_name in self.perception.joint.keys():
+            self.target_joints[joint_name] = angle
+
+    def get_posture(self):
+        '''return current posture of robot'''
+        # That sadly cant work because the recognize posture agent isn't part of the inheritance list
+
+    def execute_keyframes(self, keyframes):
+        '''excute keyframes, note this function is blocking call,
+        e.g. return until keyframes are executed
+        '''
+        self.keyframes = keyframes
+        self.angle_interpolation(keyframes, self.perception)
+        while self.current_time < self.endTime:
+            pass
+        return
+
+    def get_transform(self, name):
+        '''get transform with given name
+        '''
+        current_effected_joints = OrderedDict()
+        for joint in self.chains[name]:
+            current_effected_joints[joint] = self.perception.joint[joint]
+
+        self.forward_kinematics(name)  # calc current forward kinematics from current joint
+
+        T = [0] * len(self.chains[name])
+        for i, joint in enumerate(self.chains[name]):
+            T[i] = self.transforms[joint]  # get the solution
+
+        Te = np.array([self.from_trans(T[-1])])
+        return Te
+
+    def set_transform(self, effector_name, transform):
+        '''solve the inverse kinematics and control joints use the results
+        '''
+        keyframes = self.set_transforms(self, effector_name, transform)
+        self.execute_keyframes(keyframes)
+
+if __name__ == '__main__':
+    agent = ServerAgent()
+    server = SimpleXMLRPCServer.SimpleXMLRPCServer(('localhost', 9000), logRequests=True, allow_none=True)
+    server.register_instance(agent)
+    server.register_introspection_functions()
+    server.register_multicall_functions()
+
+    thread = threading.Thread(target=server.serve_forever)
+    thread.start()
+    agent.run()
+
